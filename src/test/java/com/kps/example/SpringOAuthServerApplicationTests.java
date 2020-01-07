382e1ccd88069e63e504ce92ef4325fe71234570
keshavendra <35345617+keshavendra@users.noreply.github.com>
2020-01-08 00:29:32 +0530

    Initial commit

---
+++ src/test/java/com/kps/example/SpringOAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kps.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
